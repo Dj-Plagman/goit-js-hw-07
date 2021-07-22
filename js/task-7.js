aabb194b100b9d9a00d6b11d1f8b29c1b378cf8f
Serhii Tsytsora <novochervone@gmail.com>
2021-07-22 21:14:11 +0300

    update_0.4

--- js/task-7.js
+++ js/task-7.js
@@ -2,9 +2,6 @@
 //скрипт, який реагує на зміну значення input#font-size-control (подія input) і змінює інлайн-стиль span#text
 //оновлюючи властивість font-size. В результаті при перетеягувані бігунка змінюється розмір тексту.
 
-<input id="font-size-control" type="range" />
-
-
 const inputRange = document.querySelector("#font-size-control");
 const text = document.querySelector("#text");
 
